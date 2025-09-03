d27005c211c3260634a5d460c0369d21efc22aa1
Yana Dziadkouskaya <yana.dziadkouskaya@gmail.com>
2025-09-03 18:13:24 +0200

    Initial commit: add eventboard, eventlistener, docker-compose, gitignore

---
+++ eventListener/src/test/java/com/dziadkouskaya/eventlistener/EventlistenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dziadkouskaya.eventlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventlistenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
